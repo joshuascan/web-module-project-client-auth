271a0b2145e74180d33aed7f47281eab5ea843e4
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-06-15 14:57:00 -0700

    Sets initial friends in FriendsList component using get request

--- friends/src/Components/FriendsList.jsx
+++ friends/src/Components/FriendsList.jsx
@@ -1,7 +1,34 @@
+import { useState, useEffect } from "react";
+import axiosWithAuth from "../utils/axiosWithAuth";
+
 const FriendsList = () => {
+  const [friends, setFriends] = useState([]);
+
+  useEffect(() => {
+    axiosWithAuth()
+      .get("/api/friends")
+      .then((res) => {
+        setFriends(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  console.log(friends);
+
   return (
     <div>
       <h2>List of Friends</h2>
+      <div>
+        {friends.map((friend) => (
+          <div key={friend.id}>
+            <p>Name: {friend.name}</p>
+            <p>Age: {friend.age}</p>
+            <p>Email: {friend.email}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
