c501ff18c33860ea7c332388d0543661437debc2
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-06-15 17:27:52 -0700

    Adds logout functionality

--- friends/src/App.js
+++ friends/src/App.js
@@ -4,15 +4,28 @@ import "./App.css";
 import Login from "./Components/Login";
 import FriendsList from "./Components/FriendsList";
 import PrivateRoute from "./Components/PrivateRoute";
+import axiosWithAuth from "./utils/axiosWithAuth";
 
 function App() {
+  const logout = () => {
+    axiosWithAuth()
+      .post("/api/logout")
+      .then((res) => {
+        localStorage.removeItem("token");
+        window.location.href = "/login";
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <Router>
       <div className="App">
         <h1>Friends App</h1>
         <Link to="/login">Login</Link>
         <Link to="/friends">Friends</Link>
-        <Link>Logout</Link>
+        <Link onClick={logout}>Logout</Link>
       </div>
 
       <Switch>
